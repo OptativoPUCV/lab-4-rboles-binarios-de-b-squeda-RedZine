de8b99e2a69d371c18839fd219501fc812322703
Your Name <you@example.com>
2025-05-01 22:24:01 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -91,7 +91,42 @@ TreeNode * minimum(TreeNode * x){
 
 
 void removeNode(TreeMap * tree, TreeNode* node) {
+    if (node == NULL) return;
 
+    // En el caso sin hijos:
+
+    if (node->left == NULL && node->right == NULL){
+        if (node->parent == NULL) tree->root == NULL;
+        else if (node->parent->left == node) node->parent->left == NULL;
+        else node->parent->right = NULL;
+    
+        free(node->pair);
+        free(node);
+    }
+    // Caso nodo con un solo hijo:
+
+    else if (node->left == NULL || node->right == NULL){
+        TreeNode *child = (node->left != NULL) ? node->left : node->right;
+        child->parent = node->parent;
+
+        if (node->parent == NULL) tree->root = child;
+
+        else if (node->parent->left == node) node->parent->left = child;
+
+        else node->parent->right = child;
+
+        free(node->pair);
+        free(node);
+    }
+
+    // Caso con dos hijitos:
+    
+    else{
+        TreeNode *minRight = minimum(node->right);
+        node->pair->key = minRight->pair->key;
+        node->pair->value = minRight->pair->value;
+        removeNode(tree, minRight);
+    }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
